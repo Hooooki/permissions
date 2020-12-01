9d4bb35061b8474f6c7b255b96310bda5f1c8885
Hoooki <hooki.kibo@gmail.com>
2020-12-01 19:22:42 +0100

    Blade provider

--- src/PermissionsServiceProvider.php
+++ src/PermissionsServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Cardinal\Permissions;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class PermissionsServiceProvider extends ServiceProvider
@@ -10,6 +11,30 @@ class PermissionsServiceProvider extends ServiceProvider
     public function boot() {
 
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
+
+        Blade::directive('permission', function ($permission) {
+
+            return "<?php if(auth()->check() && (auth()->user()->hasPermissionTo({$permission})) : ?>";
+
+        });
+
+        Blade::directive('role', function ($role) {
+
+            return "<?php if(auth()->check() && (auth()->user()->hasRole({$role})) : ?>";
+
+        });
+
+        Blade::directive('endpermission', function () {
+
+            return "<?php endif; ?>";
+
+        });
+
+        Blade::directive('endrole', function () {
+
+            return "<?php endif; ?>";
+
+        });
 
     }
 
